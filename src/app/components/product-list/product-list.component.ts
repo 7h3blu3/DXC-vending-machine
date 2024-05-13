98da66948df6d6cd215d573a8a3a55ac1a30f051
Milen Deyanov <milen.krasimirov.deyanov@gmail.com>
2024-05-13 11:03:28 +0300

    Fetching products list

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product/product.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+  // errorMessage: string;
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products = products;
+        console.log("this.products ", this.products)
+      },
+      error: (err) => {
+        // this.errorMessage = err;
+        // Optionally implement additional error handling logic here
+      }
+    });
+  }
 }
