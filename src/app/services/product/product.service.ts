21f8034863f1432c5033192515ce038c32989b74
Milen Deyanov <milen.krasimirov.deyanov@gmail.com>
2024-05-13 10:45:55 +0300

    added get products method in products service + error handling

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Product } from '../../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private apiUrl = 'http://localhost:3000/products';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Adding error handling for issues with network or backend(Imagining its not just a mocked API)
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'An unknown error has occurred.';
+    if (error.error instanceof ErrorEvent) {
+      // client side or network error
+      errorMessage = `A network error occurred: ${error.error.message}`;
+    } else {
+      // backend returned an unsuccessful response code
+      errorMessage = `Server returned code ${error.status}, message was: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
 }
